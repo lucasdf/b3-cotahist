9c08888bd868cf4dc08e70dbe58fbc78508ad378
Lucas Ferreira <github@lucasferreira.org>
2020-10-18 01:57:32 -0300

    Improve performance
    Reduce parse-file processing time with annual file from ~6 to ~1 second
    according to Criterium by replacing pmap with map (thread overhead
    wasn't worth it) and replacing bigdecimal coercion by float coercion.

--- test/b3_cotahist/core_test.clj
+++ test/b3_cotahist/core_test.clj
@@ -4,7 +4,7 @@
             [clojure.edn :as edn]
             [clojure.data.json :as json]))
 
-(deftest a-test
+(deftest core-test
   (testing "Can parse file"
     (with-redefs [core/exit identity]
       (is (= [{"ticket" "VALE5T" "price_close" 101.94 "date" "2003-02-12"}]
